b673c5f823ffe2094c894ab122bfb6380d0b991e
Anton_Bandarchyk@epam.com <Anton_Bandarchyk@epam.com>
2017-06-21 17:38:01 +0300

    lection 11 jsoup

---
+++ src/theory/lecture_11_1/jsoup/Main.java
@@ -0,0 +1,41 @@
+package theory.lecture_11_1.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * Created by Anton_Bandarchyk on 6/21/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Document doc;
+        try {
+
+            // need http protocol
+            doc = Jsoup.connect("http://google.com").get();
+
+            // get page title
+            String title = doc.title();
+            System.out.println("title : " + title);
+
+            // get all links
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+
+                // get the value from href attribute
+                System.out.println("\nlink : " + link.attr("href"));
+                System.out.println("text : " + link.text());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
